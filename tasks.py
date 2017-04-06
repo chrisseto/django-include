53569e67e2b0a66eea5dd63d96ee8cc88b05733c
Steven Loria <sloria1@gmail.com>
2017-04-06 10:09:47 -0400

    Add tasks.py

---
+++ tasks.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+import webbrowser
+
+from invoke import task
+
+docs_dir = 'docs'
+build_dir = os.path.join(docs_dir, '_build')
+
+
+@task
+def test(ctx, watch=False, last_failing=False):
+    """Run the tests.
+
+    Note: --watch requires pytest-xdist to be installed.
+    """
+    import pytest
+    flake(ctx)
+    args = []
+    if watch:
+        args.append('-f')
+    if last_failing:
+        args.append('--lf')
+    retcode = pytest.main(args)
+    sys.exit(retcode)
+
+
+@task
+def flake(ctx):
+    """Run flake8 on codebase."""
+    ctx.run('flake8 .', echo=True)
+
+
+@task
+def clean(ctx):
+    ctx.run('rm -rf build')
+    ctx.run('rm -rf dist')
+    ctx.run('rm -rf django-include.egg-info')
+    print('Cleaned up.')
+
+
+@task
+def readme(ctx, browse=False):
+    """Build README.rst. Requires Sphinx."""
+    ctx.run("rst2html.py README.rst > README.html")
+    if browse:
+        webbrowser.open_new_tab('README.html')
+
+
+@task
+def publish(ctx, test=False):
+    """Publish to the cheeseshop."""
+    clean(ctx)
+    if test:
+        ctx.run('python setup.py register -r test sdist bdist_wheel', echo=True)
+        ctx.run('twine upload dist/* -r test', echo=True)
+    else:
+        ctx.run('python setup.py register sdist bdist_wheel', echo=True)
+        ctx.run('twine upload dist/*', echo=True)
