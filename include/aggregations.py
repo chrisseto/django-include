46feaecd4c696f7fc83583868b58a87e218e9be3
John Tordoff <>
2021-01-26 13:49:58 -0500

    update django-include to Django 2.2.0

--- include/aggregations.py
+++ include/aggregations.py
@@ -5,9 +5,7 @@ from django.contrib.postgres.aggregates import ArrayAgg
 class JSONAgg(ArrayAgg):
     function = 'JSON_AGG'
     template = '%(function)s(%(expressions)s%(order_by)s)'
-
-    def __init__(self, *args, **kwargs):
-        super(JSONAgg, self).__init__(*args, output_field=JSONField(), **kwargs)
+    _output_field = JSONField()
 
     def as_sql(self, compiler, connection, function=None, template=None):
         # TODO Find a better way to handle this
@@ -15,7 +13,7 @@ class JSONAgg(ArrayAgg):
             self.extra['order_by'] = ' ORDER BY ' + ', '.join(compiler.compile(x)[0] for x in self.extra['order_by'])
         else:
             self.extra['order_by'] = ''
-        return super(JSONAgg, self).as_sql(compiler, connection, function=function, template=template)
+        return super(JSONAgg, self).as_sql(compiler, connection)
 
     def as_sqlite(self, compiler, connection, **extra_context):
         return self.as_sql(compiler, connection, function='JSON_GROUP_ARRAY', **extra_context)
